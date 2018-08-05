bdfe5d6b27539ad7c32338a6f9c4f7618c2c171b
rushi <reddyrushi20@gmail.com>
2018-08-05 23:54:00 +0530

    Predict the values from the model

--- Multiple Linear Regression/multiple_linear_regression.py
+++ Multiple Linear Regression/multiple_linear_regression.py
@@ -9,6 +9,7 @@ def main():
     data = pd.read_csv('50_Startups.csv')
     X = data.iloc[:, :-1].values
 
+    # Label encoding for categorical values
     label_encoder = LabelEncoder()
     X[:, 3] = label_encoder.fit_transform(X[:, 3])
     one_hot_encoder = OneHotEncoder(categorical_features=[3])
@@ -16,12 +17,14 @@ def main():
 
     y = data.iloc[:, -1].values
 
+    # Split the data into test and train sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
 
+    # Fit the model and predict
     regressor = LinearRegression()
     regressor.fit(X_train, y_train)
-    
 
+    y_predictions = regressor.predict(X_test)
 
 
 if __name__ == '__main__':
