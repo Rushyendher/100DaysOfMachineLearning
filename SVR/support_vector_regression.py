774faa49fb91dc2abe37b80160ede9a38dbcfef8
rushi <reddyrushi20@gmail.com>
2018-08-13 21:50:14 +0530

    Apply feature scaling

--- SVR/support_vector_regression.py
+++ SVR/support_vector_regression.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
 
@@ -9,6 +8,14 @@ def main():
     X = data.iloc[:, 1].values
     y = data.iloc[:, -1].values
 
+    # Apply feature scaling
+    scaler_X = StandardScaler()
+    scaler_y = StandardScaler()
+
+    X = scaler_X.fit_transform(X)
+    y = scaler_y.fit_transform(y)
+
+    # Fit the model
     regressor = SVR(kernel="rbf")
     regressor.fit(X)
 
